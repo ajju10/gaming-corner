467eda52f573c012a93c7f914edeaa13d8d1fd85
Ajay Yadav <yajay1257@gmail.com>
2020-06-25 08:05:19 +0530

    settings.py changed

--- etournament/settings.py
+++ etournament/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = '*h*=rf(wh_g-xod_^9ija)iws#$@cj&q8)@4g&h^nx(!+3xw61'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
