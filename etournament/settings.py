7c7c9e03f9a37478e2c34d7d074b9730dfdf1c65
Ajay Yadav <yajay1257@gmail.com>
2020-08-04 11:52:37 +0530

    Forced https

--- etournament/settings.py
+++ etournament/settings.py
@@ -91,11 +91,11 @@ MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
-# SECURE_PROXY_SSL_HEADER = (
-#     'HTTP_X_FORWARDED_PROTO', 'https'
-# )
-#
-# SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SSL_HEADER = (
+    'HTTP_X_FORWARDED_PROTO', 'https'
+)
+
+SECURE_SSL_REDIRECT = True
 
 ROOT_URLCONF = 'etournament.urls'
 
