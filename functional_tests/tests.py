d9f4cb35d69be7b092ab15f7808db40494ea1a26
Ajay Yadav <yajay1257@gmail.com>
2020-08-07 08:59:13 +0530

    StaticLiverServerTestCase instead of LiveServerTestCase in functional_tests

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,18 +1,19 @@
-from django.test import LiveServerTestCase
+import time
+
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
-from selenium.webdriver.common.keys import Keys
-import time
 
 MAX_WAIT = 10
 
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Chrome()
 
     def tearDown(self):
+        self.browser.refresh()
         self.browser.quit()
 
     def wait_for_row_in_tournament_table(self, row_text):
